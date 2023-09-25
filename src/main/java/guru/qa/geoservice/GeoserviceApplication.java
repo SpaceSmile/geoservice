5e73dd1e7dcfb7419b3fdf1296e6418cbeb0bbb8
v.sedlachek <v.sedlachek@dodopizza.com>
2023-09-25 07:23:01 +0300

    add geo

---
+++ src/main/java/guru/qa/geoservice/GeoserviceApplication.java
@@ -0,0 +1,13 @@
+package guru.qa.geoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoserviceApplication.class, args);
+    }
+
+}
